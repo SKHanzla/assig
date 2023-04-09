4864eb5ae8ee3eced2929fb83c0104f604f56dc9
Hanzla <hanzlaonmail12@gmail.com>
2023-04-09 07:34:52 +0500

    dijgi

---
+++ client/build/precache-manifest.19b74cea926327c91e48f105a2578e23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6900185c0c2e63de6a1c",
+    "url": "/static/css/main.94aa4c93.chunk.css"
+  },
+  {
+    "revision": "6900185c0c2e63de6a1c",
+    "url": "/static/js/main.6900185c.chunk.js"
+  },
+  {
+    "revision": "90c86b65ce63dabe9802",
+    "url": "/static/js/1.90c86b65.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a8e3a4da1fd4fe3439d5920905c97814",
+    "url": "/index.html"
+  }
+];
